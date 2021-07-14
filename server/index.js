5c62866a4998318e7178c5224ea6d3e4205e821f
thermiee <navyblack16@yahoo.com>
2021-07-14 22:50:50 +0100

    created model, controller and routes

--- server/index.js
+++ server/index.js
@@ -2,14 +2,18 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import todosRoutes from './routes/todos.js';
+
 const app = express();
 dotenv.config();
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
+app.use('/todos', todosRoutes);
+
 const mongodb = "mongodb+srv://mern:merntodo123@cluster0.yduiu.mongodb.net/merntodo?retryWrites=true&w=majority"
 app.get('/', (req, res) => {
     res.send('Welcome to server')
 })
-const PORT = process.env.PORT || 5000;
-mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log(`server is running on port ${PORT}`)).catch(err => console.log(err))
+const PORT = process.env.PORT || 3000;
+mongoose.connect(mongodb, { useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log(`server is running on port ${PORT}`)).catch(err => console.log(err) && console.log(e))
